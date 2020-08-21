b3a73f1b2589e0b4e9d8989881997280ec43919a
dbetka <dominik.betka@gmail.com>
2020-08-21 23:22:45 +0200

    Change version and update @dbetka/utils

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import router from './router';
 import { store } from './store';
 import { ROUTES } from 'utils/macros/routes';
 import { styleManager, THEMES } from 'utils/style-manager';
-import VueAtomic from '../../vue-atomic';
-import '../../vue-atomic/dist/themes/light.css';
-import '../../vue-atomic/dist/index.css';
 
-// Vue.component('new-input', MInput);
+// import VueAtomic from '../../vue-atomic';
+// import '../../vue-atomic/dist/themes/light.css';
+// import '../../vue-atomic/dist/index.css';
+
+import VueAtomic from '@dbetka/vue-atomic';
+import '@dbetka/vue-atomic/dist/themes/light.css';
+import '@dbetka/vue-atomic/dist/index.css';
+
 Vue.use(VueAtomic);
 
 styleManager.init();
